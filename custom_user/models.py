0d67eb8e457d2a28167f201fbfd198839cbabbca
GregoryVins <gregoryvins@gmail.com>
2021-09-09 21:29:05 +0300

    user model

--- custom_user/models.py
+++ custom_user/models.py
@@ -1,3 +1,20 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(models.Model):
+    """
+    User model.
+    """
+    id = models.UUIDField(default=uuid.uuid4, db_index=True, editable=False, primary_key=True)
+    first_name = models.CharField(max_length=255, verbose_name='Name')
+    las_name = models.CharField(max_length=255, verbose_name='Surname')
+    email = models.EmailField(verbose_name='email address', blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.las_name}'
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
